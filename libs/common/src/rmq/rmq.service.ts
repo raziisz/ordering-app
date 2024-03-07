77aca80d1088066a6476c6003d59d0059d2adcf8
Luiz Felipe <raziel_libertino@hotmail.com>
2024-03-07 13:22:59 -0400

    adicionado ack em billing

--- libs/common/src/rmq/rmq.service.ts
+++ libs/common/src/rmq/rmq.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { RmqOptions, Transport } from "@nestjs/microservices";
+import { RmqContext, RmqOptions, Transport } from "@nestjs/microservices";
 
 @Injectable()
 export class RmqService {
@@ -15,5 +15,10 @@ export class RmqService {
                 noAck
             }
         }
+    }
+    ack(context: RmqContext) {
+        const channel = context.getChannelRef();
+        const originalMessage = context.getMessage()
+        channel.ack(originalMessage);
     }
 }
